e82b0f8643086341cfab45ede6a31f783d0bcf2c
compte <stagiaire@neopolis-dev.com>
2025-08-27 11:14:19 +0100

    some changes

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,8 @@
+// ProjectCard.js
 import { Col } from "react-bootstrap";
+            import { Github } from "lucide-react";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, githubUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
@@ -8,6 +10,21 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
+          {githubUrl && (
+
+<div>
+  <a
+    href={githubUrl}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="flex items-center gap-2 text-white text-sm hover:underline"
+  >
+    <Github size={16} />
+    Voir sur GitHub
+  </a>
+</div>
+
+          )}
         </div>
       </div>
     </Col>
